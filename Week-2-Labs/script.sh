65a8746fdafd3dfe526f338fdc6dd596d143f6b0
Ken Sheridan <kensheridan86@gmail.com>
2023-05-31 10:22:40 +0100

    bash script updated

--- Week-2-Labs/script.sh
+++ Week-2-Labs/script.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-mkdir fol_1
-touch fol_1/1.1.txt
-touch fol_1/1.2.txt
-touch fol_1/1.3.txt
-mkdir fol_2
-touch fol_2/2.1.txt
-touch fol_2/2.2.txt
-touch fol_2/2.3.txt
+rm -rf fol_1
+rm -rf fol_2
