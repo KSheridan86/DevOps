fc65d0448a4acd6e98cad70d7b968df27c5d4062
Ken Sheridan <kensheridan86@gmail.com>
2023-05-31 10:30:59 +0100

    readme file created and edited

---
+++ Week-2-Labs/README.md
@@ -0,0 +1,11 @@
+# Week 2 Lab - Bash & Git
+
+Ken Sheridan - x22195441
+
+[See it Here!!](https://github.com/KSheridan86/DevOps)
+
+
+---
+
+
+To run the script.sh run: `bash script.sh`
--- Week-2-Labs/script.sh
+++ Week-2-Labs/script.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-rm -rf fol_1
-rm -rf fol_2
+mkdir fol_1
+touch fol_1/1_1.txt
+touch fol_1/1_2.txt
+touch fol_1/1_3.txt
+
+mkdir fol_2
+touch fol_2/2_1.txt
+touch fol_2/2_2.txt
+touch fol_2/2_3.txt
+
+# Set permissions for files ending with _1.txt and _3.txt in fol_1
+chmod 600 fol_1/*_1.txt
+chmod 600 fol_1/*_3.txt
+
+# Set permissions for files ending with _2.txt in fol_1 and fol_2
+chmod +rwx fol_1/*_2.txt
+chmod +rwx fol_2/*_2.txt
